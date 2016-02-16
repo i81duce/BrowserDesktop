4f10dad62bb07e556e61e367eeb10602d91da637
KevinBozic <bozic.kevin@gmail.com>
2016-02-16 11:47:48 -0500

    Added functionality and JAR file

--- src/theironyard/Controller.java
+++ src/theironyard/Controller.java
@@ -1,4 +1,63 @@
 package theironyard;
 
-public class Controller {
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Worker;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.web.WebView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable, ChangeListener {
+    @FXML
+    TextField addressBar;
+
+    @FXML
+    WebView webView;
+
+    public void goToUrl () {
+        String url = addressBar.getText();
+        if (!url.startsWith("http")) {
+            url = "http://" + url;
+        }
+        webView.getEngine().load(url);
+    }
+
+    public void onKeyPressed (KeyEvent event) {
+        if (event.getCode() == KeyCode.ENTER) {
+            goToUrl();
+        }
+    }
+
+    public void goBack () {
+        try { // Stops errors from occurring when going forward and back to far
+            webView.getEngine().getHistory().go(-1);
+        } catch (Exception e) {
+
+        }
+    }
+
+    public void goForward () {
+        try { // Stops errors from occurring when going forward and back to far
+            webView.getEngine().getHistory().go(1);
+        } catch (Exception e) {
+
+        }
+    }
+
+    @Override // Create worker object
+    public void initialize(URL location, ResourceBundle resources) {
+        Worker worker = webView.getEngine().getLoadWorker();
+        worker.stateProperty().addListener(this); // adds listener
+    }
+
+    @Override
+    public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+        addressBar.setText(webView.getEngine().getLocation());
+    }
 }
